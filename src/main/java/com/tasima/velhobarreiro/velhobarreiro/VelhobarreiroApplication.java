91a873a77300d24f44018301bf9b0c68b3dfe54d
ALLAN MOREIRA LEITE <ALU201410259@uniritter.local>
2018-06-08 19:59:14 -0300

    inicializado projeto spring

---
+++ src/main/java/com/tasima/velhobarreiro/velhobarreiro/VelhobarreiroApplication.java
@@ -0,0 +1,12 @@
+package com.tasima.velhobarreiro.velhobarreiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelhobarreiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelhobarreiroApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tasima/velhobarreiro/velhobarreiro/VelhobarreiroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tasima.velhobarreiro.velhobarreiro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VelhobarreiroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
